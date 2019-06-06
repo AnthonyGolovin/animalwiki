863486029ed9fe3381de416e0ff97f0d6f61722a
Anthony Golovin and Marqis Ramos <anthony@>
2019-06-06 11:53:40 -0700

    Fixed javascript formatting for showing and hiding information

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+var animals;
+
+$("select#animals").change(function(){
+  animals = $("select#animals").val();
+  if (animals ==1) {
+    $(".Rpanda").show();
+    $(".Lion").hide();
+      $(".Gbear").hide();
+  } else if (animals ==2) {
+    $(".Lion").show();
+    $(".Rpanda").hide();
+    $(".Gbear").hide();
+}   else if (animals ==3) {
+    $(".Gbear").show();
+    $(".Rpanda").hide();
+    $(".Lion").hide();
+}
+});
+});
